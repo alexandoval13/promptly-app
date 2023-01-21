2f35d704452394b56bd288d21d8c166a69bc9ca3
Alexandra Sandoval <alexandoval13@gmail.com>
2023-01-20 22:07:21 -0800

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+// import bodyParser from 'body-parser';
 const app = express();
 
-import { alienRoutes, testRoutes } from './routes/router';
+import {
+  alienRoutes,
+  thoughtRoutes,
+  timeRoutes,
+  timeTypeRoutes,
+  testRoutes,
+} from './routes/router';
+
+// app.use(express.static(__dirname + '/../client/dist'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/alien', alienRoutes);
+app.use('/api/thought', thoughtRoutes);
+app.use('/api/time', timeRoutes);
+app.use('/api/timeType', timeTypeRoutes);
 app.use('/api/test', testRoutes);
 
 export default app;
