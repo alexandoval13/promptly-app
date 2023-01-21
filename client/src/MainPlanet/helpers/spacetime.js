91904a0347fcc9da9ac1d48a7b512c79c30cd0bf
Alexandra Sandoval <alexandoval13@gmail.com>
2023-01-21 00:15:02 -0800

    Begin adding time helpers

---
+++ client/src/MainPlanet/helpers/spacetime.js
@@ -0,0 +1,25 @@
+import spacetime from 'spacetime';
+import daylight from 'spacetime-daylight';
+spacetime.extend(daylight);
+
+export const getCurrentDate = () => {
+  let date = spacetime.now();
+  return date;
+};
+
+export const determineDaytime = () => {
+  const sunlight = getCurrentDate().daylight();
+  return sunlight.current.status;
+};
+
+export const formatGreeting = (name: String) => {
+  const daytime: String = determineDaytime();
+
+  switch (true) {
+    case daytime === 'night':
+      return 'Good evening, ' + name;
+
+    default:
+      return 'Hello, ' + name;
+  }
+};
