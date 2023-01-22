403d418449aa4d8cbef9028d22882d5cc9204c8b
Alexandra Sandoval <alexandoval13@gmail.com>
2023-01-21 19:39:24 -0800

    Write types

---
+++ client/src/MainPlanet/types/thought.ts
@@ -0,0 +1,7 @@
+export interface Thought {
+  create_date: String;
+  id: Number;
+  thought: String;
+  update_date: String;
+  user_id: Number;
+}
---
+++ client/src/MainPlanet/types/time.ts
@@ -0,0 +1,9 @@
+export interface Time {
+  difficulty: String;
+  ex_description: String;
+  ex_name: String;
+  id: Number;
+  space_time_types: Number;
+  time_commitment: String;
+  user_id: Number;
+}
